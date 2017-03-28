7c98a07eafab7196ec86193496a7bd96d1b4f602
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-03-28 23:58:49 +0300

    Added include for cookise_manager.

--- app/view/GameCityView.php
+++ app/view/GameCityView.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
         <?php
-        spl_autoload_register();
+        include $_SERVER['DOCUMENT_ROOT'] . "\app\model\cookies_manager.php";
         logging('COOKIE', $_COOKIE);
         logging('POST', $_POST);
         if (check_post()) {
@@ -57,9 +51,9 @@ and open the template in the editor.
         }
 
         function logging($messages, $args) {
-            //echo $messages . ": ";
-            //print_r($args);
-            //echo "<br>";
+//            echo $messages . ": ";
+//            print_r($args);
+//            echo "<br>";
         }
         ?>
         <form action="" method="post">
