84a4d3ab605c03ae8d03421cfbfd5b83b14a72f0
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-04-05 00:04:52 +0400

    added new class game

--- app/model/cookies_manager.php
+++ app/model/cookies_manager.php
@@ -1,38 +1,55 @@
 <?php
 
 /**
- * Description of cookies_manager
- *
+ * This manager help save and read element in the array from the cookies.
  * @author HNKNTOC
  */
-class cookies_manager {
+class cookies_array_manager {
 
-    private $city_names = array();
+    private $array = array();
+    private $cookie_name;
+
+    function __construct($cookie_name) {
+        $this->cookie_name = $cookie_name;
+    }
 
     function get_city_names() {
-        return $this->city_names;
+        return $this->array;
     }
 
     function init() {
         if ($this->check_cookies()) {
-            $this->city_names = unserialize($_COOKIE["CITY_NAMES"]);
+            $this->array = unserialize($_COOKIE[$this->cookie_name]);
         }
+        logging("init array", $this->array);
     }
 
-    function addCity($newCity) {
-        array_push($this->city_names, $newCity);
+    function add($new_value) {
+        array_push($this->array, $new_value);
+    }
+
+    function add_at_index($new_value, $index) {
+        $this->array[$index] = $new_value;
+    }
+
+    function get_value($index) {
+        return $this->array[$index];
     }
 
     function save() {
-        setcookie("CITY_NAMES", serialize($this->city_names));
+        setcookie($this->cookie_name, serialize($this->array));
     }
 
     function clear() {
         $_COOKIE = array();
     }
+    
+    function check_exists($index){
+        return array_key_exists($index, $this->array);
+    }
 
     private function check_cookies() {
-        return array_key_exists('CITY_NAMES', $_COOKIE);
+        return array_key_exists($this->cookie_name, $_COOKIE);
     }
 
 }
---
+++ app/model/game.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Description of game
+ *
+ * @author HNKNTOC
+ */
+class game {
+    /*
+     * Use for save and get status name city in cookies.
+     */
+
+    private $manager_status;
+
+    /**
+     * This array with cities names.
+     * @var type Array.
+     */
+    private $array_citys_name;
+    private $file_path;
+
+    function __construct() {
+        $this->manager_status = new cookies_array_manager("CITY_STATUS");
+        $this->manager_status->init();
+        $this->file_path = $_SERVER['DOCUMENT_ROOT'] . "\app\\res\list_city";
+    }
+
+    function init() {
+        $file = fopen($this->file_path, "r");
+        $string_city_names = fread($file, filesize($this->file_path));
+        $this->array_citys_name = explode(",", $string_city_names);
+
+        for ($i = 0; $i < sizeof($this->array_citys_name); $i++) {
+            $this->manager_status->add_at_index(1, $i);
+        }
+    }
+
+    function check_word($word) {
+        $in_array = in_array($word, $this->array_citys_name);
+        if ($in_array) {
+            $index = $this->get_index_for_name_city($word);
+            $result = $this->check_word_status($index);
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    //TODO: delete check_word_status. hi in check_word.
+    function say_word($word) {
+        if ($this->check_word($word)) {
+            $index = $this->get_index_for_name_city($word);
+            if ($this->check_word_status($index)) {
+                $this->manager_status->add_at_index(0, $index);
+                $this->manager_status->save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function get_index_for_name_city($search_name) {
+        while ($city_name = current($this->array_citys_name)) {
+            if ($city_name == $search_name) {
+                return key($this->array_citys_name);
+            }
+            next($this->array_citys_name);
+        }
+    }
+
+    //TODO bug in get_status_for_name_city
+    function get_status_for_name_city($index_city_name) {
+        $exists = $this->manager_status->check_exists($index_city_name);
+        if ($exists) {
+            $this->manager_status->init();
+            $result = $this->manager_status->get_value($index_city_name);
+            logging("result status", $result);
+            return $result;
+        } else {
+            logging("get_status_for_name_city", "return 0");
+            return 0;
+        }
+    }
+
+    private function check_word_status($index_city_name) {
+        $status = $this->get_status_for_name_city($index_city_name);
+        logging("status", $status);
+        if ($status == 1) {
+            logging("check_word_status", "true");
+            return true;
+        } else {
+            logging("check_word_status", "false");
+            return false;
+        }
+    }
+
+}
